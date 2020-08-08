6679747e0c769bd83a514abb4bc9b391764bf8f2
tosinfarai <tosinfarai@gmail.com>
2020-08-08 19:32:49 +0100

    initial commit

---
+++ omod/src/main/java/org/openmrs/module/conceptname/web/controller/ConceptNameController.java
@@ -0,0 +1,39 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.conceptname.web.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.Concept;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class ConceptNameController {
+ 
+ @ModelAttribute
+ public Concept getConcept(@RequestParam(required=false, value="conceptId")Concept concept){
+ return concept;
+ }
+ @GetMapping("/module/conceptname/viewConceptNames")
+ public void showForm(){
+ 
+ }
+}
